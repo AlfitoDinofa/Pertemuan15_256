69577fcb5633a469f3f30eab73751ea0562cdd7a
AlfitoDinofa <alfitodinofa2006@gmail.com>
2026-06-22 01:29:46 +0700

    membuat connection di dalam dal.cs

---
+++ CRUDMahasiswaADO/DAL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CRUDMahasiswaADO
+{
+    class DAL
+    {
+        static string connectionString = @"Data Source=LAPTOP-IF142S7G\ALFITO; Initial Catalog=DBAkademikADO; Integrated Security=True";
+
+        public string GetConnectionString()
+        {
+            return connectionString;
+        }
+
+        SqlConnection conn = new SqlConnection(connectionString);
+        SqlDataAdapter da;
+        DataTable dtMahasiswa;
+        DataTable dtProdi;
+
+ 
+    }
+}
